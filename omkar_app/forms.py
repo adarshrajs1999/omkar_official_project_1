fee36c13a4fd6a065bc581213b6cab9a7407c93c
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-10-08 12:53:51 +0530

    today

--- omkar_app/forms.py
+++ omkar_app/forms.py
@@ -1,6 +1,8 @@
 # booking/forms.py
+from email.headerregistry import Group
+
 from django import forms
-from .models import Room, Booking
+from .models import Room, Booking, Couple_Room, Family_Room, Group_Room, Six_Bed_Room, Dormitory
 
 
 class AvailabilityForm(forms.Form):
@@ -24,3 +26,98 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['name', 'address', 'phone', 'email','check_in','check_out']
+
+
+class CoupleRoomForm(forms.ModelForm):
+    class Meta:
+        model = Couple_Room  # Link the form to the Couple_Room model
+        fields = '__all__'  # Use all fields in the model, or you can specify certain fields like ['Room_name', 'Room_amount', ...]
+
+        # Add customizations to widgets if necessary
+        widgets = {
+            'Room_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Name'}),
+            'Room_amount': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Amount'}),
+            'Check_in': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Check_out': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your Name'}),
+            'Phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Phone Number'}),
+            'Email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter Email Address'}),
+            'Adults': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Adults'}),
+            'Child': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Children'}),
+        }
+
+
+class CoupleRoomForm(forms.ModelForm):
+    class Meta:
+        model = Family_Room  # Link the form to the Couple_Room model
+        fields = '__all__'  # Use all fields in the model, or you can specify certain fields like ['Room_name', 'Room_amount', ...]
+
+        # Add customizations to widgets if necessary
+        widgets = {
+            'Room_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Name'}),
+            'Room_amount': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Amount'}),
+            'Check_in': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Check_out': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your Name'}),
+            'Phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Phone Number'}),
+            'Email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter Email Address'}),
+            'Adults': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Adults'}),
+            'Child': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Children'}),
+        }
+
+
+class CoupleRoomForm(forms.ModelForm):
+    class Meta:
+        model = Group_Room  # Link the form to the Couple_Room model
+        fields = '__all__'  # Use all fields in the model, or you can specify certain fields like ['Room_name', 'Room_amount', ...]
+
+        # Add customizations to widgets if necessary
+        widgets = {
+            'Room_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Name'}),
+            'Room_amount': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Amount'}),
+            'Check_in': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Check_out': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your Name'}),
+            'Phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Phone Number'}),
+            'Email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter Email Address'}),
+            'Adults': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Adults'}),
+            'Child': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Children'}),
+        }
+
+
+class CoupleRoomForm(forms.ModelForm):
+    class Meta:
+        model = Six_Bed_Room  # Link the form to the Couple_Room model
+        fields = '__all__'  # Use all fields in the model, or you can specify certain fields like ['Room_name', 'Room_amount', ...]
+
+        # Add customizations to widgets if necessary
+        widgets = {
+            'Room_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Name'}),
+            'Room_amount': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Amount'}),
+            'Check_in': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Check_out': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your Name'}),
+            'Phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Phone Number'}),
+            'Email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter Email Address'}),
+            'Adults': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Adults'}),
+            'Child': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Children'}),
+        }
+
+
+class CoupleRoomForm(forms.ModelForm):
+    class Meta:
+        model = Dormitory  # Link the form to the Couple_Room model
+        fields = '__all__'  # Use all fields in the model, or you can specify certain fields like ['Room_name', 'Room_amount', ...]
+
+        # Add customizations to widgets if necessary
+        widgets = {
+            'Room_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Name'}),
+            'Room_amount': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter Room Amount'}),
+            'Check_in': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Check_out': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'Name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your Name'}),
+            'Phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Phone Number'}),
+            'Email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter Email Address'}),
+            'Adults': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Adults'}),
+            'Child': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of Children'}),
+        }
