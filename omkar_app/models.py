f2308d678af5a6687204a81f511c6feaf9bf7190
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-10-08 12:30:03 +0530

    today

--- omkar_app/models.py
+++ omkar_app/models.py
@@ -29,3 +29,59 @@ class Booking(models.Model):
 
     def _str_(self):
         return f"Booking for {self.name} - {self.room}"
+
+
+class Couple_Room(models.Model):
+    Room_name=models.CharField(max_length=1000)
+    Room_amount=models.IntegerField()
+    Check_in=models.DateField()
+    Check_out=models.DateField()
+    Name=models.CharField(max_length=100)
+    Phone=models.IntegerField()
+    Email=models.EmailField()
+    Adults=models.IntegerField()
+    Child=models.IntegerField()
+
+class Family_Room(models.Model):
+    Room_name = models.CharField(max_length=1000)
+    Room_amount = models.IntegerField()
+    Check_in = models.DateField()
+    Check_out = models.DateField()
+    Name = models.CharField(max_length=100)
+    Phone = models.IntegerField()
+    Email = models.EmailField()
+    Adults = models.IntegerField()
+    Child = models.IntegerField()
+
+class Group_Room(models.Model):
+    Room_name=models.CharField(max_length=1000)
+    Room_amount=models.IntegerField()
+    Check_in=models.DateField()
+    Check_out=models.DateField()
+    Name=models.CharField(max_length=100)
+    Phone=models.IntegerField()
+    Email=models.EmailField()
+    Adults=models.IntegerField()
+    Child=models.IntegerField()
+
+class Six_Bed_Room(models.Model):
+    Room_name=models.CharField(max_length=1000)
+    Room_amount=models.IntegerField()
+    Check_in=models.DateField()
+    Check_out=models.DateField()
+    Name=models.CharField(max_length=100)
+    Phone=models.IntegerField()
+    Email=models.EmailField()
+    Adults=models.IntegerField()
+    Child=models.IntegerField()
+
+class Dormitory(models.Model):
+    Room_name=models.CharField(max_length=1000)
+    Room_amount=models.IntegerField()
+    Check_in=models.DateField()
+    Check_out=models.DateField()
+    Name=models.CharField(max_length=100)
+    Phone=models.IntegerField()
+    Email=models.EmailField()
+    Adults=models.IntegerField()
+    Child=models.IntegerField()
