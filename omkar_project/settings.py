f47b88e4fb892ca1b3978402eba3768adba53599
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-10-24 14:43:39 +0530

    today

--- omkar_project/settings.py
+++ omkar_project/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'omkar_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "omkar_official_project_1_database",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        # "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
